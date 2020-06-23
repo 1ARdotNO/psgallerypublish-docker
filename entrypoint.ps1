367f88879ce5d336e72db050ec3662def5e41087
Einar Stenberg <einar@stenberg.im>
2020-06-23 16:09:58 +0200

    Create entrypoint.ps1

---
+++ entrypoint.ps1
@@ -0,0 +1,15 @@
+  
+[cmdletbinding()]
+param()
+
+$outputMsg = '**Publishing Module**'
+if ($env:INPUT_TAGS) {
+    $outputMsg += "`nTags were provided`n" + ($env:INPUT_TAGS -split "," | Format-List | Out-String -Width 80).Trim()
+}
+if ($env:INPUT_APIKEY) {
+    $outputMsg += "`nAPI Key was provided too`n" + ($env:INPUT_APIKEY)
+}
+
+$outputMsg += "`nCurrent path on this container: $($pwd)`n"
+
+Write-Output $outputMsg
